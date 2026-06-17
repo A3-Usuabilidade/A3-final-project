ac12a293a6b89cf8b17f3c9d37a8c6d2392edbd9
Will <willianfigueiredodev@gmail.com>
2026-06-17 19:34:26 -0300

    feat: finaliza catalogo da loja

---
+++ a3-usabilidade-src/src/assets/brand/index.js
@@ -0,0 +1,2 @@
+export { default as logoNexus } from './logo-nexus.svg';
+export { default as logoNexusWhite } from './logo-nexus-white.svg';
--- a3-usabilidade-src/src/layouts/LayoutApp.jsx
+++ a3-usabilidade-src/src/layouts/LayoutApp.jsx
@@ -1,11 +1,7 @@
 import { Outlet } from 'react-router-dom';
-import ThemeToggle from '../componentes/ThemeToggle.jsx';
 
 export default function LayoutApp() {
   return (
-    <>
-      <Outlet />
-      <ThemeToggle />
-    </>
+    <Outlet />
   );
 }
